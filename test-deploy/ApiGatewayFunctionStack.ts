d908c0a8ab94845efafd2cf70d0fc9fee10bf55c
Andy Blackledge <andyblackledge@hotmail.com>
2021-03-11 21:41:32 +0000

    Got API with API key working

--- test-deploy/ApiGatewayFunctionStack.ts
+++ test-deploy/ApiGatewayFunctionStack.ts
@@ -3,7 +3,8 @@
 /* eslint-disable no-new */
 import * as cdk from '@aws-cdk/core';
 import * as apigateway from '@aws-cdk/aws-apigateway';
-import * as lambda from '@aws-cdk/aws-lambda';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs';
+import path from 'path';
 
 export default class ApiGatewayFunctionStack extends cdk.Stack {
   //
@@ -11,21 +12,30 @@ export default class ApiGatewayFunctionStack extends cdk.Stack {
     //
     super(scope, id, props);
 
-    const handler = new lambda.Function(this, 'WidgetHandler', {
-      runtime: lambda.Runtime.NODEJS_10_X, // So we can use async in widget.js
-      code: lambda.Code.fromAsset('resources'),
-      handler: 'widgets.main',
+    const portmanteauFunction = new lambda.NodejsFunction(this, 'GetRequestFunction', {
+      entry: path.join(__dirname, '.', 'functions', 'ApiGatewayFunctions.ts'),
+      handler: 'portmanteauHandler',
     });
 
-    const api = new apigateway.RestApi(this, 'widgets-api', {
-      restApiName: 'Widget Service',
-      description: 'This service serves widgets.',
+    const api = new apigateway.RestApi(this, 'ApiGatewayFunction', {
+      restApiName: 'ApiGatewayFunction Test Service',
+      apiKeySourceType: apigateway.ApiKeySourceType.HEADER,
     });
 
-    const getWidgetsIntegration = new apigateway.LambdaIntegration(handler, {
-      requestTemplates: { 'application/json': '{ "statusCode": "200" }' },
+    const apiKey = new apigateway.ApiKey(this, 'ApiKey');
+
+    new apigateway.UsagePlan(this, 'UsagePlan', {
+      apiKey,
+      apiStages: [{ stage: api.deploymentStage }],
     });
 
-    api.root.addMethod('GET', getWidgetsIntegration); // GET /
+    const getRequestIntegration = new apigateway.LambdaIntegration(portmanteauFunction);
+    api.root.addMethod('GET', getRequestIntegration, {
+      apiKeyRequired: true,
+    }); // GET /
+
+    new cdk.CfnOutput(this, 'ApiKeyID', {
+      value: apiKey.keyId,
+    });
   }
 }
---
+++ test-deploy/functions/ApiGatewayFunctions.ts
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable import/prefer-default-export */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import Log from '@dazn/lambda-powertools-logger';
+import CorrelationIds from '@dazn/lambda-powertools-correlation-ids';
+import middyCorrelationIds from '@dazn/lambda-powertools-middleware-correlation-ids';
+import { Context } from 'aws-lambda/handler';
+import { ApiGatewayFunction } from '../../src';
+
+const correlationIdParams = { sampleDebugLogRate: 0.01 };
+
+ApiGatewayFunction.Log = Log;
+ApiGatewayFunction.getCorrelationIds = CorrelationIds.get;
+
+class PortmanteauRequest {
+  x: string;
+
+  y: string;
+}
+
+class PortmanteauResponse {
+  result: string;
+}
+
+class PortmanteauFunction extends ApiGatewayFunction<PortmanteauRequest, PortmanteauResponse> {
+  async handleRequestAsync(request: PortmanteauRequest): Promise<PortmanteauResponse> {
+    return { result: `${request.x}+${request.y}` };
+  }
+}
+
+const portmanteauFunction = new PortmanteauFunction();
+
+export const portmanteauHandler = middy(
+  async (event: any, context: Context): Promise<any> =>
+    portmanteauFunction.handleAsync(event, context)
+)
+  .use(middyCorrelationIds(correlationIdParams))
+  .use(httpErrorHandler());
